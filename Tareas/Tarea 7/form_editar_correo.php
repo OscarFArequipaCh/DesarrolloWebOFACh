8c84857920c3f55f5e19acc085aa7d77b4cc9535
OscarFArequipaCh <102692025+OscarFArequipaCh@users.noreply.github.com>
2025-03-31 23:52:41 -0400

    ultimos arreglos sbre estilos para formulario de actualizacion de correos

--- Tareas/Tarea 7/form_editar_correo.php
+++ Tareas/Tarea 7/form_editar_correo.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Editar Cprreo</title>
+    <title>Editar Correo</title>
+    <link rel="stylesheet" href="stylesform.css">
 </head>
 <body>
     <?php
@@ -13,11 +14,17 @@
     $resultado=$con->query($sql);
     $row = $resultado->fetch_assoc();
     ?>
-    <form action="editcorreo.php" method="post">
-        <label for="nombres">Nombres y Apellidos: <?php echo $row['nombres'];?> <?php echo $row['apellidos'];?></label><br>
-        <label for="correo">Correo:</label>
-        <input type="email" name="correo" value="<?php echo $row['correo'];?>"><br>
-        <input type="submit" value="Guardar">
-    </form>
+    <div class="container">
+        <form action="editcorreo.php" method="post">
+            <p><strong>Nombres y Apellidos:</strong> <?php echo $row['nombres'];?> <?php echo $row['apellidos'];?></p>
+            
+            <div class="form-group">
+                <label for="correo">Correo:</label>
+                <input type="email" name="correo" value="<?php echo $row['correo'];?>">
+            </div>
+            
+            <input type="submit" value="Actualizar">
+        </form>
+    </div>
 </body>
 </html>
